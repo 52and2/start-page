1d681ab4a8378a96d7f2347f9e0c7873ed4dfdc7
52and2 <71156954+52and2@users.noreply.github.com>
2025-12-10 03:24:23 +0000

    Update browser information retrieval in useFetchData

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -39,7 +39,8 @@ const useFetchData = () => {
 			theme: settings.theme.name,
 			time: moment().format(settings.fetch.timeFormat),
 			date: moment().format(settings.fetch.dateFormat),
-			browser: LibreWolf,
+			browser: result.browser.name
+		,
 			browserLower: result.browser.name.toLowerCase(),
 			browserVersion: result.browser.version,
 			engineName: result.engine.name,
